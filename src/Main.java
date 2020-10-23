b556a106c776e31d98a1722970016a94102a715a
nastya <sobakipovodok@gmail.com>
2020-10-23 18:27:36 +0300

    ended 1 lesson

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();//вывод в консоль любой строчки
+        System.out.println("hello world");
+    }
+
+}
